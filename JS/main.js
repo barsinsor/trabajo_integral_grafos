0ff051a5495d3d5890b14135cca828950447f4eb
Luis Salinas <35043719+LuisSP97@users.noreply.github.com>
2020-07-17 14:59:55 -0400

    Se agrega asignacion de camiones segun cantidad de cd's

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,28 @@
 window.onload = function() {
-    console.log('main.js')
-
     function leerArchivo(e) {
         var archivo = e.target.files[0];
         if (!archivo) {
             return;
         }
-        var lector = new FileReader();
-        lector.onload = function(e) {
-            var contenido = e.target.result;
-            mostrarContenido(contenido);
-            splitContent(contenido)
-        };
-        lector.readAsText(archivo);
+        try {
+            if (archivo.type == 'text/plain') {
+                var lector = new FileReader()
+                lector.onload = function(e) {
+                    limpiarAsignacion()
+                    var contenido = e.target.result
+                    mostrarContenido(contenido)
+                    contenido = splitContenido(contenido)
+                    let count = countCD(contenido)
+                    asginarCamiones(count)
+                }
+            }
+            lector.readAsText(archivo);
+        } catch (error) {
+            limpiarAsignacion()
+            console.log('¡El archivo seleccionado no tiene el formato correcto!')
+            var elemento = document.getElementById('contenido-archivo')
+            elemento.innerHTML = '¡El archivo seleccionado no es de texto plano, por favor, seleccione un archivo con el formato correcto!'
+        }
     }
 
     function mostrarContenido(contenido) {
@@ -20,13 +30,42 @@ window.onload = function() {
         elemento.innerHTML = contenido;
     }
 
-    function splitContent(contenido) {
+    function splitContenido(contenido) {
         arrContenido = contenido.split('\n')
         arrContenidoFix = []
         for (let i = 0; i < arrContenido.length; i++) {
             arrContenidoFix[i] = arrContenido[i].split(';')
         }
         console.log(arrContenidoFix)
+        return arrContenidoFix
+    }
+
+    function countCD(contenido) {
+        let count = 0
+        for (let i = 0; i < contenido.length; i++)
+            for (let j = 0; j < contenido.length; j++) {
+                if (contenido[i][j] == 'c' || contenido[i][j] == 'C') {
+                    count++
+                }
+            }
+        console.log('cantidad centros de distribucion:' + count)
+        return count
+    }
+
+    function asginarCamiones(cantidad) {
+        for (let i = 0; i != cantidad; i++) {
+            var div = document.createElement('div')
+            div.innerHTML = `<form><div class="form-row"><div class="col"><label>Centro distribuidor${i + 1}</label></div><div class="col"><input id="camion${i}" type="text" class="form-control" placeholder="Numero de camion"></div><div class="col"><input id="cantidad${i}" type="text" class="form-control" placeholder="Cantidad de productos"></div></div></form>`
+            document.getElementById('asignarCamiones').appendChild(div)
+        }
+        var btn = document.createElement('btn')
+        btn.innerHTML = '<button type="submit" class="btn btn-light">Aceptar</button>'
+        document.getElementById('asignarCamiones').appendChild(btn)
+    }
+
+    function limpiarAsignacion() {
+        var elemento = document.getElementById('asignarCamiones')
+        elemento.innerHTML = ''
     }
 
     document.getElementById('file-input')
